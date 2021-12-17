a74f4dca287bf4e12d5cc6aa1aa19c0cc8f4696e
nazar12314 <kononenkonazar47@gmail.com>
2021-12-17 11:06:28 +0200

    Finished functions repeat and product

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """Itertools module"""
 from typing import Iterable
-import doctest
 
 
 def count(start=0, step=1):
@@ -12,16 +11,32 @@ def count(start=0, step=1):
 def cycle(iterable):
     pass
 
-def repeat(value, amount=None):
-    if amount is None:
-        while True:
-            yield value
-    else:
-        for _ in range(amount):
-            yield value
+class repeat:
+    """repeat
+    Repeat(object [,times]) -> create an iterator which returns the object for the specified number of times.
+    If 'amount' is not specified, returns the object endlessly
+
+    The following examples shows the execution and result in list format.
+    >>> list(repeat(10, 7))
+    [10, 10, 10, 10, 10, 10, 10]
+    >>> list(repeat(5, 1))
+    [5]
+    """
+    def __init__(self, value, amount=None):
+        self.value = value
+        self.amount = amount
+    def __iter__(self):
+        if self.amount is None:
+            while True:
+                yield self.value
+        else:
+            for _ in range(self.amount):
+                yield self.value
+    def __str__(self):
+        return f"repeat({self.value}, {self.amount if self.amount is not None else 'inf'})"
 
 
-def product(*args: Iterable, repeat=1):
+class product:
     """Product
     Returns cartesian product of input iterables
     Receives as a parameter an array of data, consisting of several groups of values.
@@ -32,7 +47,7 @@ def product(*args: Iterable, repeat=1):
     specify the number of repetitions with the optional 'repeat' keyword argument.
     For example, product(A, repeat=4) means the same as product(A, A, A, A).
 
-    The following examples shows the execution of this function and result in list format.
+    The following examples shows the execution and result in list format.
     >>> list(product('ABC', '123'))
     [('A', '1'), ('A', '2'), ('A', '3'), ('B', '1'), \
 ('B', '2'), ('B', '3'), ('C', '1'), ('C', '2'), ('C', '3')]
@@ -41,20 +56,26 @@ def product(*args: Iterable, repeat=1):
     >>> list(product(repeat=10))
     [()]
     """
-    # Function works recursively
-    if not args:
-        # Base case. If args iterator is empty - return ()
-        yield tuple()
-    else:
-        # Include repeat case
-        args *= repeat
-        # Choose starting point
-        element = args[0]
-        # Iterate through elements in order to create combinations
-        for item in element if callable(element) else iter(element):
-            # 
-            for elements in product(*args[1:]):
-                yield (item, ) + elements
+    def __init__(self, *args, repeat=1):
+        self.args = args
+        self.repeat = repeat
+    def __iter__(self):
+        # Function works recursively
+        if not self.args:
+            # Base case. If args iterator is empty - return ()
+            yield tuple()
+        else:
+            # Include repeat case
+            self.args *= self.repeat
+            # Choose starting point
+            element = self.args[0]
+            # Iterate through elements in order to create combinations
+            for item in element if callable(element) else iter(element):
+                # 
+                for elements in product(*self.args[1:]):
+                    yield (item, ) + elements
+    def __str__(self):
+        return f"Product(args: {self.args}, repeat={self.repeat})"
 
 
 def permutations(iterable, length=None):
