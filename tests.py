d8c4d93191117bf5ec044c2cd9f0473923ea4c2c
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2021-12-20 14:37:47 +0200

    added tests for combinations_with_replacement

--- tests.py
+++ tests.py
@@ -16,4 +16,16 @@ assert list(main.permutations('abc', 2)) == [('a', 'b'), ('a', 'c'), ('b', 'a'),
 assert list(main.permutations(['a', 'b', 'c'], 2)) == [('a', 'b'), ('a', 'c'), ('b', 'a'), ('b', 'c'), ('c', 'a'), ('c', 'b')]
 assert list(main.permutations([['a'], ['b']], 3)) == []
 assert list(main.permutations('abc', 1)) == [('a',), ('b',), ('c',)]
+assert list(main.combinations_with_replacement('ABC', 2)) == [('A', 'A'), ('A', 'B'), ('A', 'C'),
+                                                              ('B', 'B'), ('B', 'C'), ('C', 'C')]
+assert list(main.combinations_with_replacement(['1', '2', '3', '4'], 3)) == [
+    ('1', '1', '1'), ('1', '1', '2'), ('1', '1', '3'), ('1', '1', '4'),
+    ('1', '2', '2'), ('1', '2', '3'), ('1', '2', '4'), ('1', '3', '3'),
+    ('1', '3', '4'), ('1', '4', '4'), ('2', '2', '2'), ('2', '2', '3'),
+    ('2', '2', '4'), ('2', '3', '3'), ('2', '3', '4'), ('2', '4', '4'),
+    ('3', '3', '3'), ('3', '3', '4'), ('3', '4', '4'), ('4', '4', '4')
+]
+assert len(list(main.combinations_with_replacement([element for element in range(100)], 3))) == 171700
+assert list(main.combinations_with_replacement('ABC', 0)) == [()]
+assert list(main.combinations_with_replacement('', 10)) == []
 
